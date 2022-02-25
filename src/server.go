8c0712f974a444f52c05e979a472f576251125d8
samithiwat <samithiwat2544@gmail.com>
2022-02-25 07:23:06 +0700

    Using config and connect to db

--- src/server.go
+++ src/server.go
@@ -1,13 +1,37 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/samithiwat/samithiwat-backend/src/config"
+	"github.com/samithiwat/samithiwat-backend/src/database"
+)
 
 func main() {
+    config, err := config.LoadConfig(".")
+    
+    if err != nil {
+        log.Fatal("cannot to load config", err)
+    }
+
+    client, err := database.InitDatabase()
+    
+    if err != nil {
+        log.Fatal("cannot to init database", err)
+    }
+
+    err = client.AutoMigrate()
+    
+    if err != nil {
+        log.Fatal("cannot migrate database", err)
+    }
+
     app := fiber.New()
 
     app.Get("/", func(c *fiber.Ctx) error {
         return c.SendString("Hello, World 👋!")
     })
 
-    app.Listen(":3000")
+    app.Listen(":" + config.Port)
 }
