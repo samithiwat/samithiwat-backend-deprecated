199cc52d1bc00c286d4b4f56cc04be4f4a06ab4d
samithiwat <samithiwat2544@gmail.com>
2022-02-25 19:44:09 +0700

    Create Icon Image and Settings Model

---
+++ src/model/icon.model.go
@@ -0,0 +1,21 @@
+package model
+
+import (
+	"time"
+
+	"github.com/samithiwat/samithiwat-backend/src/common/enum"
+	"gorm.io/gorm"
+)
+
+type Icon struct {
+	ID 		  int64 		 `json:"id"`
+	Name      string 		 `json:"icon"`
+	BgColor   string 		 `json:"icon_bg_color"`
+	IconType  enum.IconType  `json:"icon_type"`
+	OwnerID     int 			 `json:"owner_id"`
+	OwnerType string 		 `json:"owner_type"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at"`
+
+}
---
+++ src/model/image.model.go
@@ -0,0 +1,20 @@
+package model
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Image struct {
+	ID          int64     `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	ImgUrl      string    `json:"img_url"`
+	OwnerID     int       `json:"owner_id"`
+	OwnerType   string    `json:"owner_type"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at"`
+
+}
