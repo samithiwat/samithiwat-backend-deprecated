13b0cec81b274ec13b76292a0e4c66d610ac5e06
samithiwat <samithiwat2544@gmail.com>
2022-02-28 07:39:33 +0700

    Implement decoration service with resovler

--- src/graph/resolver/decoration.resolver.go
+++ src/graph/resolver/decoration.resolver.go
@@ -6,6 +6,8 @@ package graph
 import (
 	"context"
 	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"strconv"
 	"time"
 
 	"github.com/samithiwat/samithiwat-backend/src/graph/generated"
@@ -17,47 +19,111 @@ func (r *badgeResolver) Icon(ctx context.Context, obj *model.Badge) (*model.Icon
 }
 
 func (r *badgeResolver) DeletedAt(ctx context.Context, obj *model.Badge) (*time.Time, error) {
+
 	panic(fmt.Errorf("not implemented"))
 }
 
 func (r *iconResolver) IconType(ctx context.Context, obj *model.Icon) (string, error) {
-	panic(fmt.Errorf("not implemented"))
+	iconType, err := r.decorationService.CheckIconType(obj.IconType)
+	if err != nil{
+		return "", err
+	}
+
+	return iconType, nil
 }
 
 func (r *iconResolver) DeletedAt(ctx context.Context, obj *model.Icon) (*time.Time, error) {
-	panic(fmt.Errorf("not implemented"))
+	if !obj.DeletedAt.Time.IsZero() {
+		return nil, fiber.ErrNotFound
+	}
+
+	return &obj.DeletedAt.Time, nil
 }
 
 func (r *mutationResolver) CreateIcon(ctx context.Context, newIcon model.NewIcon) (*model.Icon, error) {
-	panic(fmt.Errorf("not implemented"))
+	icon, err := r.decorationService.CreateIcon(newIcon)
+	if err != nil {
+		return nil, err
+	}
+
+	return icon, nil
 }
 
 func (r *mutationResolver) UpdateIcon(ctx context.Context, id string, newIcon model.NewIcon) (*model.Icon, error) {
-	panic(fmt.Errorf("not implemented"))
+	convertedID, err := strconv.Atoi(id)
+
+	icon, err := r.decorationService.UpdateIcon(int64(convertedID), newIcon)
+	if err != nil {
+		return nil, err
+	}
+
+	return icon, nil
 }
 
 func (r *mutationResolver) DeleteIcon(ctx context.Context, id string) (*model.Icon, error) {
-	panic(fmt.Errorf("not implemented"))
+	convertedID, err := strconv.Atoi(id)
+
+	icon, err := r.decorationService.DeleteIcon(int64(convertedID))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return icon, nil
 }
 
 func (r *mutationResolver) CreateBadge(ctx context.Context, newBadge *model.NewBadge) (*model.Badge, error) {
-	panic(fmt.Errorf("not implemented"))
+	badge, err := r.decorationService.CreateBadge(newBadge)
+	if err != nil {
+		return nil, err
+	}
+
+	return badge, nil
 }
 
 func (r *mutationResolver) UpdateBadge(ctx context.Context, id string, newBadge *model.NewBadge) (*model.Badge, error) {
-	panic(fmt.Errorf("not implemented"))
+	convertedID, err := strconv.Atoi(id)
+
+	badge, err := r.decorationService.UpdateBadge(int64(convertedID), newBadge)
+	if err != nil {
+		return nil, err
+	}
+
+	return badge, nil
 }
 
 func (r *mutationResolver) DeleteBadge(ctx context.Context, id string) (*model.Badge, error) {
-	panic(fmt.Errorf("not implemented"))
+	convertedID, err := strconv.Atoi(id)
+
+	badge, err := r.decorationService.DeleteBadge(int64(convertedID))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return badge, nil
 }
 
 func (r *queryResolver) Icons(ctx context.Context) ([]*model.Icon, error) {
-	panic(fmt.Errorf("not implemented"))
+	icons, err := r.decorationService.GetAllIcons()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return icons, nil
 }
 
 func (r *queryResolver) Icon(ctx context.Context, id string) (*model.Icon, error) {
-	panic(fmt.Errorf("not implemented"))
+	convertedID, err := strconv.Atoi(id)
+
+	icon, err := r.decorationService.GetIcon(int64(convertedID))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return icon, nil
 }
 
 func (r *queryResolver) IconsByOwner(ctx context.Context, ownerID int, ownerType string) ([]*model.Icon, error) {
