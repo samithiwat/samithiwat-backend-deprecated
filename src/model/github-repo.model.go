3c5d436ba95afd64018ec1f6cdf03c5151939a9b
samithiwat <samithiwat2544@gmail.com>
2022-03-07 03:25:56 +0700

    Reformat code

--- src/model/github-repo.model.go
+++ src/model/github-repo.model.go
@@ -7,17 +7,17 @@ import (
 )
 
 type GithubRepo struct {
-	ID        	 int64     	 	 `json:"id"`
-	Name	  	 string    	 	 `json:"name"`
-	Author	  	 string    	 	 `json:"author"`
-	Description  string    	 	 `json:"description"`
-	ThumbnailUrl string    	 	 `json:"thumbnail_url"`
-	Url 	 	 string    	 	 `json:"url"`
-	LatestUpdate time.Time 	 	 `json:"latest_update"`
-	Star      int64     `json:"star"`
-	Framework Badge     `json:"framework" gorm:"polymorphic:Owner;polymorphicValue:framework"`
-	Language  Badge     `json:"language" gorm:"polymorphic:Owner;polymorphicValue:language"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at"`
+	ID           int64          `json:"id"`
+	Name         string         `json:"name"`
+	Author       string         `json:"author"`
+	Description  string         `json:"description"`
+	ThumbnailUrl string         `json:"thumbnail_url"`
+	Url          string         `json:"url"`
+	LatestUpdate time.Time      `json:"latest_update"`
+	Star         int64          `json:"star"`
+	Framework    Badge          `json:"framework" gorm:"polymorphic:Owner;polymorphicValue:framework"`
+	Language     Badge          `json:"language" gorm:"polymorphic:Owner;polymorphicValue:language"`
+	CreatedAt    time.Time      `json:"created_at"`
+	UpdatedAt    time.Time      `json:"updated_at"`
+	DeletedAt    gorm.DeletedAt `json:"deleted_at"`
 }
