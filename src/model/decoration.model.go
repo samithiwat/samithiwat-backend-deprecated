113fb5a176dcde8c5cfd4caf23117b280ab5097e
samithiwat <samithiwat2544@gmail.com>
2022-03-07 03:00:25 +0700

    Change model directory

---
+++ src/model/decoration.model.go
@@ -0,0 +1,32 @@
+package model
+
+import (
+	"time"
+
+	"github.com/samithiwat/samithiwat-backend/src/common/enum"
+	"gorm.io/gorm"
+)
+
+type Icon struct {
+	ID        int64          `json:"id"`
+	Name      string         `json:"icon"`
+	BgColor   string         `json:"icon_bg_color"`
+	IconType  enum.IconType  `json:"icon_type"`
+	OwnerID   int64          `json:"owner_id" gorm:"default:null"`
+	OwnerType string         `json:"owner_type"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at"`
+}
+
+type Badge struct {
+	ID        int64          `json:"id"`
+	Name      string         `json:"name"`
+	Color     string         `json:"color"`
+	Icon      Icon           `json:"icon" gorm:"polymorphic:Owner;"`
+	OwnerID   int64          `json:"owner_id" gorm:"default:null"`
+	OwnerType string         `json:"owner_type"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at"`
+}
--- src/model/github-repo.model.go
+++ src/model/github-repo.model.go
@@ -14,10 +14,10 @@ type GithubRepo struct {
 	ThumbnailUrl string    	 	 `json:"thumbnail_url"`
 	Url 	 	 string    	 	 `json:"url"`
 	LatestUpdate time.Time 	 	 `json:"latest_update"`
-	Star	 	 int64     	 	 `json:"star"`
-	Framework    Badge 	 	 	 `json:"framework" gorm:"polymorphic:Owner;polymorphicValue:framework"`
-	Language	 Badge 	 	 	 `json:"language" gorm:"polymorphic:Owner;polymorphicValue:language"`
-	CreatedAt 	 time.Time 	 	 `json:"created_at"`
-	UpdatedAt 	 time.Time 	 	 `json:"updated_at"`
-	DeletedAt 	 gorm.DeletedAt  `json:"deleted_at"`
+	Star      int64     `json:"star"`
+	Framework Badge     `json:"framework" gorm:"polymorphic:Owner;polymorphicValue:framework"`
+	Language  Badge     `json:"language" gorm:"polymorphic:Owner;polymorphicValue:language"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at"`
 }
